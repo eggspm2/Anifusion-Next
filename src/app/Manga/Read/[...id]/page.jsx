6db17aea3a2ed19001670b5344f5330fbcb03aa5
reyyuuki <irfan31331@gmail.com>
2024-08-01 23:55:20 +0530

    fixing error of not deploying

--- src/app/Manga/Read/[...id]/page.jsx
+++ src/app/Manga/Read/[...id]/page.jsx
@@ -5,8 +5,6 @@ import {
   faBook,
   faChevronLeft,
   faChevronRight,
-  faLeftLong,
-  faRightLong,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
@@ -23,21 +21,19 @@ import {
 import { Button } from "@/components/ui/button";
 
 const page = ({ params }) => {
-  const [data, setData] = useState("");
-  const [chapter, setChapter] = useState(params.id[1]);
-  const [prev, setPrev] = useState(true);
-  const [next, setNext] = useState(true);
+  const [Data, setData] = useState("");
+  const [Chapter, setChapter] = useState(params.id[1]);
+  const [Prev, setPrev] = useState(true);
+  const [Next, setNext] = useState(true);
 
   useEffect(() => {
     const Fetching = async () => {
-      if (chapter) {
-        const response = await MangaChapter(params.id[0] + "/" + chapter);
+      if (Chapter) {
+        const response = await MangaChapter(params.id[0] + "/" + Chapter);
         if (response) {
           console.log(response);
           setData(response);
-          const index = response.chapterListIds.findIndex(
-            (item) => item.name === response.currentChapter
-          );
+          const index = response.chapterListIds.findIndex((item) => item.name === response.currentChapter);
           console.log(index);
           if (index === 0) {
             setNext(false);
@@ -53,34 +49,27 @@ const page = ({ params }) => {
       }
     };
     Fetching();
-  }, [chapter, next, prev]);
+  }, [Chapter, Next, Prev]);
 
   const handleChapter = (nextbtn) => {
     if (nextbtn) {
-      if (next) {
-        const index =
-          data.chapterListIds.findIndex(
-            (item) => item.name === data.currentChapter
-          ) - 1;
-        setChapter(data.chapterListIds[index].id);
+      if (Next) {
+        const index = Data.chapterListIds.findIndex((item) => item.name === Data.currentChapter) - 1;
+        setChapter(Data.chapterListIds[index].id);
         if (index === 0) {
           setNext(false);
         }
-        console.log(chapter, index);
       }
     } else {
-      if (prev) {
+      if (Prev) {
         const index =
-          data.chapterListIds.findIndex(
-            (item) => item.name === data.currentChapter
-          ) + 1;
-        setChapter(data.chapterListIds[index].id);
-        if (index === data.chapterListIds.length - 1) {
+          Data.chapterListIds.findIndex((item) => item.name === Data.currentChapter) + 1;
+        setChapter(Data.chapterListIds[index].id);
+        if (index === Data.chapterListIds.length - 1) {
           setPrev(false);
         } else {
           setPrev(true);
         }
-        console.log(chapter, index);
       }
     }
   };
@@ -90,18 +79,18 @@ const page = ({ params }) => {
     
   };
 
-  if (!data) return <div>Loading ...</div>;
+  if (!Data) return <div>Loading ...</div>;
   return (
     <div className="flex flex-col items-center w-full gap-3 pt-4">
       <div className="flex flex-col gap-2 justify-center items-center">
-        <h1 className="text-[30px]">{data.title}</h1>
-        <h2 className="text-[20px]">{data.currentChapter}</h2>
+        <h1 className="text-[30px]">{Data.title}</h1>
+        <h2 className="text-[20px]">{Data.currentChapter}</h2>
       </div>
       <div className="flex w-[70%] justify-between items-center gap-2">
         <Button
           variant="outline"
           className={` p-[10px] ${
-            prev ? "" : "bg-white text-gray-500/50 hover:bg-white"
+            Prev ? "" : "bg-white text-gray-500/50 hover:bg-white"
           }`}
           onClick={() => handleChapter(false)}
         >
@@ -128,7 +117,7 @@ const page = ({ params }) => {
               <DrawerClose>
                 <div className="flex flex-col gap-5 items-center">
                   <div className="flex flex-col items-center w-full gap-2 h-[300px] overflow-y-scroll ScrollWidth p-[20px]">
-                    {data.chapterListIds.map((item) => (
+                    {Data.chapterListIds.map((item) => (
                       <Button
                         variant="outline"
                         key={item.id}
@@ -150,7 +139,7 @@ const page = ({ params }) => {
         <Button
           variant="outline"
           className={` p-[10px] ${
-            next ? "" : "bg-white text-gray-500/50 hover:bg-white"
+            Next ? "" : "bg-white text-gray-500/50 hover:bg-white"
           }`}
           onClick={() => handleChapter(true)}
         >
@@ -158,7 +147,7 @@ const page = ({ params }) => {
         </Button>
       </div>
       <div className="flex flex-col w-full justify-center items-center py-[20px]">
-        {data.images.map((item, index) => (
+        {Data.images.map((item, index) => (
           <img
             src={item.image}
             alt=""
