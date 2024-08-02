d57bc5df6a1e0571e843dec0ddb7151e4015f805
reyyuuki <irfan31331@gmail.com>
2024-08-02 23:59:16 +0530

    Update SearchResults.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -19,20 +19,20 @@ const SearchResults = ({ result }) => {
         <Link
           href={`/Anime/home/${item.id}`}
           key={item.id}
-          className="flex w-full h-[100px] bg-zinc-700/50 border border-zinc-500/50 rounded-xl p-[10px]"
+          className="flex w-full h-[110px] bg-zinc-700/50 border border-zinc-500/50 rounded-xl p-[10px]"
         >
           <img
             src={item.poster}
             alt=""
-            className="w-[60px] h-full object-cover rounded-xl border border-zinc-500/50"
+            className="w-[80px] h-full object-cover rounded-xl border border-zinc-500/50"
           />
-          <div className="flex flex-col justify-center items-center w-full gap-4">
+          <div className="flex flex-col justify-center items-center w-full gap-1">
             <h1 className="text-[18px] font-semibold">
               {item.name.length > 20
                 ? item.name.substring(0, 18) + "..."
                 : item.name}
             </h1>
-            <div className="flex items-center gap-2 w-min bg-white text-black p-2 rounded-lg">
+            <div className="flex items-center justify-evenly gap-2 w-[250px] bg-black Box_Shadow  p-1 text-[14px] rounded-lg">
               <div className="flex justify-center items-center gap-1">
                 <FontAwesomeIcon icon={faTv} />
                 <p>{item.type || "??"}</p>
@@ -49,10 +49,6 @@ const SearchResults = ({ result }) => {
               <div className="flex justify-center items-center gap-1">
                 <FontAwesomeIcon icon={faMicrophone} />
                 <p>{item.episodes.sub || "??"}</p>
-              </div>
-              <div className="flex justify-center items-center gap-1">
-                <FontAwesomeIcon icon={faStar} />
-                <p>{item.rating / 10 || "??"}</p>
               </div>
             </div>
           </div>
