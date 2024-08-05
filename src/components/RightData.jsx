5f4f2119eb2c1e4617d3b5cf604d09a60ae5c33b
reyyuuki <irfan31331@gmail.com>
2024-08-05 15:29:21 +0530

    Update RightData.jsx

--- src/components/RightData.jsx
+++ src/components/RightData.jsx
@@ -11,23 +11,23 @@ export const RightData = ({ data ,height}) => {
     <div className="flex flex-col w-full gap-5  h-[100%]">
       {data.relatedAnimes && (
         <div className="flex flex-col gap-2 p-[10px] border border-zinc-500/50 rounded-lg ">
-          <div className="flex gap-3 items-center">
+          <div className="flex gap-3 items-center text-[30px] max-md:text-[25px] font-semibold">
             <FontAwesomeIcon icon={faLayerGroup}/>
-          <h1 className="text-[30px] font-semibold max-md:text-[25px]">Related Anime</h1>
+          <h1>Related Anime</h1>
           </div>
           <div className="overflow-y-scroll ScrollWidth max-h-[400px] flex flex-col gap-2">
-          <Table data={data.relatedAnimes} width={'w-[170px] text-[12px]'} />
+          <Table data={data.relatedAnimes} width={'w-[180px] text-[12px]'} />
           </div>
         </div>
       )}
       {data.mostPopularAnimes.length > 0 && (
         <div className="flex flex-col gap-2 p-[10px]  border border-zinc-500/50  rounded-lg">
-          <div className="flex gap-3 items-center">
+          <div className="flex gap-3 items-center text-[30px] font-semibold max-md:text-[25px]">
             <FontAwesomeIcon icon={faFire}/>
-          <h1 className="text-[30px] font-semibold max-md:text-[25px]">Most Popular</h1>
+          <h1>Most Popular</h1>
           </div>
           <div className={`flex flex-col gap-2 ${height} overflow-y-scroll ScrollWidth`}>
-          <Table data={data.mostPopularAnimes} width={'w-[170px] text-[12px]'}/>
+          <Table data={data.mostPopularAnimes} width={'w-[180px] text-[12px]'}/>
           </div>
         </div>
       )}
